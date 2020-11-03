351d66e517e3378864538642d61b15454a3362e6
lunabartolozzi <luna.bartolozzi@gmail.com>
2020-11-03 00:14:56 -0300

    actualizacion

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 const precios = [100, 200, 300, 400, 500, 600, 700];
 
 const presupuesto = {
-  precioTotal: 0,
+    precioTotal: 0,
 };
 
 function obtenerPresupuesto() {
-  var nombreProyecto = prompt('Ingrese el nombre de su proyecto');
-  alert('Gracias por el pedido para ' + nombreProyecto);
 
-  var checkboxes = document.getElementsByName('checkbox');
-  var posicion = 0;
-  for (var checkbox of checkboxes) {
-    if (checkbox.checked) {
-      presupuesto.precioTotal = presupuesto.precioTotal + precios[posicion];
+    document.getElementById("boton").style.backgroundColor = "green";
+
+
+    var nombreProyecto = prompt('Ingrese el nombre de su proyecto');
+    alert('Gracias por el pedido para ' + nombreProyecto);
+
+    var checkboxes = document.getElementsByName('checkbox');
+    var posicion = 0;
+    for (var checkbox of checkboxes) {
+        if (checkbox.checked) {
+            presupuesto.precioTotal = presupuesto.precioTotal + precios[posicion];
+        }
+        posicion++;
     }
-    posicion++;
-  }
-  document.getElementById('precio').innerHTML = presupuesto.precioTotal;
+    document.getElementById('precio').innerHTML = presupuesto.precioTotal;
+
+
+
 }
